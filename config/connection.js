5f0cf72d9d434b6e0f6fea2c26111973a2ef6db0
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-18 00:00:08 -0800

    added the some set up files with some node packages and set up the connection.js files

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql")
+
+var connection = mysql.createConnection({
+    host:"local",
+    port: 3306,
+    user:"root",
+    password:"password",
+    database:""
+})
+
+connection.connect((err)=>{
+    if(err){
+        console.err("err conneciton "+resizeBy.stack)
+        return;
+    }
+    console.log("conected as id "+ connection.threadId)
+})
+
+
+module.exports = connection;
