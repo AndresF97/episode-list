d771f3b8efd4fbfc45ebf2e6082c098b0e47dd5d
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-18 00:30:32 -0800

    set up the sql files

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS episode_db;
+
+CREATE DATABASE episode_db;
+
+USE episode_db;
+
+
+CREATE TABLE episode(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    show_name VARCHAR(100),
+    episode_name VARCHAR(100),
+    watched BIT(1),
+    PRIMARY KEY(id)
+);
+INSERT INTO episode (show_name,episode_name) VALUES ("HOUSE MD","WILSON HEART HOUSES MIND");
+INSERT INTO episode (show_name,episode_name) VALUES ("My Hero Academia","Red Riot");
+INSERT INTO episode (show_name,episode_name) VALUES ("The Man in the highcastle","pilot");
+INSERT INTO episode (show_name,episode_name) VALUES ("Archer","pilot");
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO episode (show_name,episode_name) VALUES ("HOUSE MD","WILSON HEART HOUSES MIND");
+INSERT INTO episode (show_name,episode_name) VALUES ("My Hero Academia","Red Riot");
+INSERT INTO episode (show_name,episode_name) VALUES ("The Man in the highcastle","pilot");
+INSERT INTO episode (show_name,episode_name) VALUES ("Archer","pilot");
