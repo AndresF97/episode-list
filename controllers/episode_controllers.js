5a4aa66e6b5afa03b0c32bb4c254224640d7c67d
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-19 22:41:55 -0800

    set up the model folder

---
+++ controllers/episode_controllers.js
@@ -0,0 +1,37 @@
+var express = require("express")
+var router  = express.Router();
+var shows = require("../model/episode.js")
+
+router.get("/",function(req,res){
+    shows.selectAll(function(data){
+        var obj = {
+            shows: data
+        };
+        res.render("index",obj)
+    });
+});
+
+router.post("/api/shows",function(req,res){
+    shows.insertOne(["name","comedy"],[req.body.name, req.body.comedy],function(result){
+        res.json({id: result.inserId})
+    });
+});
+
+router.put("/api/shows/:id",function(req,res){
+    var con  = "id = " +  req.params.id;
+    console.log("condition",con)
+    shows.upDateOne(
+        {
+            comedy : req.body.comedy
+        },
+        con,
+        function(results){
+            if(results.changedRows === 0){
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+})
+
+module.exports = router;
---
+++ model/episode.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orms.js");
+
+var show  ={
+    selectAll : function(cb){
+        orm.selectAll("episode",function(res){
+            cb(res);
+        });
+    },
+    insertOne : function(info,cb){
+        orm.insertOne("episode",info,function(res){
+            cb(res)
+        });
+    },
+    upDateOne: function(col,val,cb){
+        orm.updateOne("episode",col,val, function(res){
+            cb(res)
+        })
+    }
+};
+
+module.exports = show;
+
