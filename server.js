5de387b7e9844ef3eed9543bfbfe372f218f3ae1
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-19 23:02:30 -0800

    SET UP the server.js file

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+
+var app = express()
+
+var PORT = process.env.PORT || 3000;
+
+var exphbs = require("express-handlebars");
+
+var routes = require("./controllers/episode_controllers.js")
+
+
+
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended : true}));
+
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout:"main"}));
+app.set("view engine","handelbars");
+
+
+app.use(routes)
+
+
+app.listen(PORT,function(){
+    console.log("App now listening at localhost:" + PORT)
+});
