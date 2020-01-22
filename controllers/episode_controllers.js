3adc047573a2790aad1454d217191b211120f6be
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-21 21:31:41 -0800

    added some html to the handlebars

--- controllers/episode_controllers.js
+++ controllers/episode_controllers.js
@@ -4,20 +4,16 @@ var shows = require("../model/episode.js")
 
 router.get("/",function(req,res){
     shows.selectAll(function(data){
-        // var wObject ={
-        //     episode: data
-        // };
-        console.log(data);
         res.render("index",{wObject:data});
     });
 
 });
 
-// router.post("/api/shows",function(req,res){
-//     shows.insertOne(["name","comedy"],[req.body.name, req.body.comedy],function(result){
-//         res.json({id: result.inserId})
-//     });
-// });
+router.post("/api/shows",function(req,res){
+    shows.insertOne(["name","comedy"],[req.body.name, req.body.comedy],function(result){
+        res.json({id: result.inserId})
+    });
+});
 
 // router.put("/api/shows/:id",function(req,res){
 //     console.log(req.params.id)
