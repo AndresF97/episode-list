fecaa5d8ed37ab9c7ca477041047ca107c669280
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-22 22:05:25 -0800

    Updated the connections text in the connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql")
-var connection
+var connection;
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
-    connection = mysql.createConnection({
-    host:"ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+ connection = mysql.createConnection({
+    host:"localhost",
     port: 3306,
-    user:"kkkycim08khv8xda",
-    password:"oipgyrfqetq0qvgl",
+    user:"root",
+    password:"password",
     database:"fv2m5mixpq1o7ldf"
-})
+   });
+};
 
 connection.connect(function(err){
     if(err){
