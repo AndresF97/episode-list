a9707267aa4eff415d26a58715f4dfb9bea80038
Andres J <Andres.F.Jimenez.F@gmail.com>
2020-08-23 22:53:23 -0700

    updated db to actually save info from user

--- db/schema.sql
+++ db/schema.sql
@@ -10,9 +10,10 @@ CREATE TABLE episode(
     show_name VARCHAR(100),
     episode_name VARCHAR(100),
     watched TINYINT(1) NOT NULL Default"1",
+    memo VARCHAR(500),
     PRIMARY KEY(id)
 );
-INSERT INTO episode (show_name,episode_name) VALUES ("HOUSE MD","WILSON HEART HOUSES MIND");
-INSERT INTO episode (show_name,episode_name) VALUES ("My Hero Academia","Red Riot");
-INSERT INTO episode (show_name,episode_name) VALUES ("The Man in the highcastle","pilot");
-INSERT INTO episode (show_name,episode_name) VALUES ("Archer","pilot");
+INSERT INTO episode (show_name,episode_name,memo) VALUES ("HOUSE MD","WILSON HEART HOUSES MIND","Sad");
+INSERT INTO episode (show_name,episode_name,memo) VALUES ("My Hero Academia","Red Riot","awesome");
+INSERT INTO episode (show_name,episode_name,memo) VALUES ("The Man in the highcastle","pilot","want to watch more");
+INSERT INTO episode (show_name,episode_name,memo) VALUES ("Archer","pilot","Funny");
