4e481f6ae9fe5fd9965dcf91bbcd663219a90d2a
Andres J <Andres.F.Jimenez.F@gmail.com>
2020-10-13 12:27:45 -0700

    added away to save the url image into mysql that way we dont alway have to make a call to API

--- db/schema.sql
+++ db/schema.sql
@@ -10,10 +10,11 @@ CREATE TABLE episode(
     show_name VARCHAR(100),
     episode_name VARCHAR(100),
     watched BOOLEAN DEFAULT true,
+    show_logo VARCHAR(600),
     memo VARCHAR(500),
     PRIMARY KEY(id)
 );
-INSERT INTO episode (show_name,episode_name,memo) VALUES ("HOUSE MD","WILSON HEART HOUSES MIND","Sad");
-INSERT INTO episode (show_name,episode_name,memo) VALUES ("My Hero Academia","Red Riot","awesome");
-INSERT INTO episode (show_name,episode_name,memo) VALUES ("The Man in the highcastle","pilot","want to watch more");
-INSERT INTO episode (show_name,episode_name,memo) VALUES ("Archer","pilot","Funny");
+INSERT INTO episode (show_name,episode_name,memo,show_logo) VALUES ("HOUSE MD","WILSON HEART HOUSES MIND","Sad","https://m.media-amazon.com/images/M/MV5BMDA4NjQzN2ItZDhhNC00ZjVlLWFjNTgtMTEyNDQyOGNjMDE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg");
+INSERT INTO episode (show_name,episode_name,memo,show_logo) VALUES ("My Hero Academia","Red Riot","awesome","https://m.media-amazon.com/images/M/MV5BNmQzYmE2MGEtZjk4YS00YmVjLWEwZWMtODRkMjc4MTM5N2I3XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg");
+INSERT INTO episode (show_name,episode_name,memo,show_logo) VALUES ("The Man in the highcastle","pilot","want to watch more","https://m.media-amazon.com/images/M/MV5BZWEwNzQ4NzUtMWRmYS00NDdiLTg5NDItODA5M2M4YTM0ZTE2XkEyXkFqcGdeQXVyMTAzNjU2NjM1._V1_SX300.jpg");
+INSERT INTO episode (show_name,episode_name,memo,show_logo) VALUES ("Archer","pilot","Funny","https://m.media-amazon.com/images/M/MV5BZWEwNzQ4NzUtMWRmYS00NDdiLTg5NDItODA5M2M4YTM0ZTE2XkEyXkFqcGdeQXVyMTAzNjU2NjM1._V1_SX300.jpg");
